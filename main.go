186d0b869640aea25c80d4b053571f71a5bd1763
marcusyu-curative <marcus.yu@gmail.com>
2025-02-19 17:37:48 -0800

    remove comments for gin related code

--- main.go
+++ main.go
@@ -15,47 +15,10 @@ func main() {
 	InitDatabase()
 	defer DB.Close()
 
-	// e := gin.Default()
-	// e.LoadHTMLGlob("templates/*")
 	mux := http.NewServeMux()
-
-	// e.GET("/", func(c *gin.Context) {
-	// 	todos := ReadToDoList()
-	// 	c.HTML(http.StatusOK, "index.html", gin.H{
-	// 		"todos": todos,
-	// 	})
-	// })
 	mux.Handle("/", loggingMiddleware(http.HandlerFunc(indexHandler)))
-
-	// e.POST("/todos", func(c *gin.Context) {
-	// 	title := c.PostForm("title")
-	// 	status := c.PostForm("status")
-	// 	id, _ := CreateToDo(title, status)
-
-	/* c.HTML leverages Go native templating */
-	// c.HTML(http.StatusOK, "task", gin.H{
-	// 	"Title":  title,
-	// 	"Status": status,
-	// 	"Id":     id,
-	// })
-
-	// 	c.Writer.Header().Set("Content-Type", "text/html; charset=utf-8")
-	// 	c.Status(http.StatusOK)
-	// 	component := components.Task(models.ToDo{Id: id, Title: title, Status: status})
-	// 	if err := component.Render(c.Request.Context(), c.Writer); err != nil {
-	// 		c.String(http.StatusInternalServerError, err.Error())
-	// 	}
-	// })
 	mux.Handle("POST /todos", loggingMiddleware(http.HandlerFunc(createToDoHandler)))
-
-	// e.DELETE("/todos/:id", func(c *gin.Context) {
-	// 	param := c.Param("id")
-	// 	id, _ := strconv.ParseInt(param, 10, 64)
-	// 	DeleteToDo(id)
-	// })
 	mux.Handle("DELETE /todos/{id}", loggingMiddleware(http.HandlerFunc(deleteToDoHandler)))
-
-	//e.Run(":8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
